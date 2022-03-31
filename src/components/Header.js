6b3eafb5b14605de8ee28e7ebaccbdba3b6ee9d6
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-03-31 15:42:07 +0300

    feature: signOut

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React from "react";
 import headerLogo from "../images/logo-mesto.svg";
-import {Link, Route} from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 
-function Header(props) {
-    return (
-        <header className="header">
-            <img src={headerLogo} alt="Логотип Место" className="header__logo"/>
-            <div>
-                <Route path="/sign-in">
-                    <Link to='sign-up' className='header__link'>Регистрация</Link>
-                </Route>
-                <Route path="/sign-up">
-                    <Link to='sign-in' className='header__link'>Войти</Link>
-                </Route>
-                <Route path='/'>
-                    <p>{props.email}</p>
-                </Route>
-            </div>
-        </header>
-    );
+function Header({email, onSignOut}) {
+  return (
+    <header className="header">
+      <img src={headerLogo} alt="Логотип Место" className="header__logo" />
+      <div>
+        <Route path="/sign-up">
+          <Link to="sign-in" className="header__link">
+            Войти
+          </Link>
+        </Route>
+        <Route path="/sign-in">
+          <Link to="sign-up" className="header__link">
+            Регистрация
+          </Link>
+        </Route>
+        <Route exact path="/">
+          <div className="header__info">
+            <p className="header__email">{email}</p>
+            <button
+              onClick={onSignOut
+              }
+              className="header__link header__button"
+            >
+              Выйти
+            </button>
+          </div>
+        </Route>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
